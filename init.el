c741eb4f30091c1312c63ccb144102366c41766b
alv-r- <no>
2014-10-17 20:17:05 -0300

    AC and yasnippet working

--- init.el
+++ init.el
@@ -19,11 +19,39 @@
 ;; Remove toolbar
 (tool-bar-mode -1)
 
+;; Turn off word wrapping
+(setq-default truncate-lines 1)
+
 ;; Auto linum-mode
 (global-linum-mode 1)
 
 ;; Theme
 (load-theme 'zenburn t)
+
+;; yasnippet
+;; should be loaded before auto complete so that they can work together
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Fix yasnippet 0.8/ac bug
+(defalias 'yas/get-snippet-tables 'yas--get-snippet-tables)
+(defalias 'yas/table-hash 'yas--table-hash)
+
+;; auto complete mode
+;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;; set the trigger key so that it can work together with yasnippet on tab key,
+;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+
+;; auto-complete-clang
+(require 'auto-complete-clang)
+(setq ac-sources (append '(ac-source-clang) ac-sources))
+
 
 ;; PgDown/PgUp bindings:
 (require 'smartrep)
